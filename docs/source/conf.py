ffdc58ffa8a250dca7fb0566a9616793e0707cc3
yuqcheng <chengyuq@outlook.com>
2023-11-09 19:28:39 -0500

    readthedocs logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ html_theme = "sphinx_book_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_logo = "../img/epipack_logo.png"
+html_logo = "_static/epipack_logo.png"
 html_theme_options = {
     #"logo_only": True,
     "show_toc_level": 2,

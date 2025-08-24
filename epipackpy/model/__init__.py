b4dda897bbf63cb8de9b4736abffa952b540326e
yuqcheng <chengyuq@outlook.com>
2025-08-24 17:07:09 -0400

    package update

--- epipackpy/model/__init__.py
+++ epipackpy/model/__init__.py
@@ -3,5 +3,5 @@ from ..model.peak_model import Peak_Model
 from ..model.utils import query_model_initial
 from ..model.PEIVI_mapping import PEIVI_MAPPING
 from ..model.classifier import Classifier
-from .global_oor import global_oor_query3
+from .global_oor import global_oor_query
 from local_oor import local_oor_detector
